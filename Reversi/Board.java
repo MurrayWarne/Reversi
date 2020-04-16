8451e1693af36874b6ddd0bdfa0e34fa9d9ff5f8
Murray Warne <murr.warne@gmail.com>
2020-04-16 19:44:20 +0100

    Refactored player panel to its own method

---
+++ Reversi/Board.java
@@ -0,0 +1,17 @@
+
+/**
+ * Board Class
+ *
+ * @author Murray Warne
+ * @version V1.0
+ */
+public class Board
+{
+
+    /**
+     * Constructor for objects of class Board
+     */
+    public Board()
+    {
+    }
+}
--- Reversi/Reversi.java
+++ Reversi/Reversi.java
@@ -23,13 +23,41 @@ public class Reversi
     private void makeFrame()
     {
         JFrame frame = new JFrame("Reversi"); // Creates a frame named reversi
-        JPanel score = new JPanel(new BorderLayout()); // Creates the JPanel to hold the score and player names
+        JPanel player = makePlayerPanel(); // Sets the Player Panel to a jpanel called player
         JPanel board = new JPanel(new BorderLayout()); // Creates the JPanel to hold the game board
         JLabel status = new JLabel("Please enter your names and press start to continue"); // Label to hold the status bar
-        frame.add(score, BorderLayout.EAST); // Adds the score to the right of the frame
+        frame.add(player, BorderLayout.EAST); // Adds the score to the right of the frame
         frame.add(board, BorderLayout.CENTER); // adds the board to the left of the frame
         frame.add(status,BorderLayout.SOUTH); // Adds the status to the bottom of the board
         frame.pack();
         frame.setVisible(true); // Makes the frame visible
+    }
+    
+    /**
+     * Creates the player panel so it can be added to the frame
+     */
+    private JPanel makePlayerPanel()
+    {
+        JPanel player = new JPanel();// Creates the JPanel to hold the score and player names
+        // Holds the inputs for player names
+        JOptionPane player1 = new JOptionPane("Enter Player 1 Here"); 
+        JOptionPane player2 = new JOptionPane("Enter Player 2 Here"); 
+        // Creates the labels to hold player scores
+        JLabel score1 = new JLabel("Player 1's Score"); 
+        JLabel score2 = new JLabel("Player 2's Score");
+        // Creates the labels to hold the number of discs each player has
+        JLabel discs1 = new JLabel("Player 1's Discs");
+        JLabel discs2 = new JLabel("Player 2's Discs");
+        //Creates the button to start the game
+        JButton play = new JButton("Play"); // Creates a new button called play
+        // Adds all of the elements to the panel
+        player.add(player1); 
+        player.add(score1);
+        player.add(discs1);
+        player.add(player2); 
+        player.add(score2);
+        player.add(discs2);
+        player.add(play); 
+        return player; // returns the JPanel
     }
 }
