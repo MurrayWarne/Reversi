6f4abcb8fa94c02f5e45895256fa4b0f4f1d21e7
Murray Warne <murr.warne@gmail.com>
2020-04-16 18:38:45 +0100

    Added the basic layout
    Added the basic board layout, including the game board, status bar and score board.
    Also commented over the code

--- Reversi/Reversi.java
+++ Reversi/Reversi.java
@@ -18,15 +18,18 @@ public class Reversi
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * This is used to create the frame used for the main game board
      */
     private void makeFrame()
     {
-        JFrame frame = new JFrame("Reversi");
+        JFrame frame = new JFrame("Reversi"); // Creates a frame named reversi
+        JPanel score = new JPanel(new BorderLayout()); // Creates the JPanel to hold the score and player names
+        JPanel board = new JPanel(new BorderLayout()); // Creates the JPanel to hold the game board
+        JLabel status = new JLabel("Please enter your names and press start to continue"); // Label to hold the status bar
+        frame.add(score, BorderLayout.EAST); // Adds the score to the right of the frame
+        frame.add(board, BorderLayout.CENTER); // adds the board to the left of the frame
+        frame.add(status,BorderLayout.SOUTH); // Adds the status to the bottom of the board
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible(true); // Makes the frame visible
     }
 }
